198e1e05533371a8e47f5545c659f8b33f548b54
Edward Huang <ejhuang.2015@gmail.com>
2021-05-07 15:32:40 -0400

    Converted Navbar into React component

---
+++ src/components/AboutMe/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function AboutMe() {
+
+    return (
+        <div className="centered" id="aboutMe">
+          <h3 className="sectionTitle">About</h3>
+          <div className="columns is-vcentered">
+            <div className="empty column is-1"></div>
+            <div className="column is-5 container">
+              <header className="about">Bio</header>
+              <hr className="smallHR"/>
+              <p className="content">
+                `An aspiring full stack web developer with strong interests in design and Asian media.
+                Passionate about video games and Japanese culture with a desire to one day move to Japan and work for closely
+                with Nintendo.
+                RPG styled games where customization is plentiful are what I enjoy the most but my favorite series is Fire
+                Emblem.
+                I love comedies and fantasy, prefer cute and fluffy, and am an absolute coward when it comes to horror.`
+              </p>
+            </div>
+            <div className="column is-5 container">
+              <header className="about">Attributes</header>
+              <hr className="smallHR"/>
+              <div className="columns about">
+                <div className="column is-6">
+                  <header className="about underlined">Traits</header>
+                  <ul>
+                    <li>Well Organized</li>
+                    <li>Honest</li>
+                    <li>Team-Oriented</li>
+                  </ul>
+                </div>
+                <div className="column is-6">
+                  <header className="about underlined">Skills</header>
+                  <ul>
+                    <li>Javascript</li>
+                    <li>C++</li>
+                    <li>Adobe Photoshop</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="empty column is-1"></div>
+          </div>
+        </div>
+    )
+}
+
+export default AboutMe;
---
+++ src/components/WebPortfolio/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { webProjects } from "../../common/portfolioProjects";
+
+function WebPortfolio() {
+
+    return (
+
+    )
+}
+
+export default WebPortfolio;
