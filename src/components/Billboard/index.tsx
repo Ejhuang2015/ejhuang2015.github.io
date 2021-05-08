2f79ddcf4330d16bbf645593dc09867d087402c7
Edward Huang <ejhuang.2015@gmail.com>
2021-05-08 11:53:08 -0400

    Updated all pages

--- src/components/Billboard/index.tsx
+++ src/components/Billboard/index.tsx
@@ -1,31 +1,59 @@
-import React from "react";
-import headshot from "../../assets/Headshot.jpg"
+// Billboard Component
+// =============================================================
 
+// Dependencies
+// =============================================================
+import React from "react";
+import { IContactLink } from "../../common/types"
+import { contactLinks } from "../../common/navLinks";
+import headshot from "../../assets/Headshot.jpg"
+import emailIcon from "../../assets/email.png";
+
+// Main
+// =============================================================
 function Billboard() {
 
+  // Map Functions
+  // =============================================================
+  // Link Items
+  function mapContacts(item: IContactLink) {
     return (
-        <div className="columns centered title reverseRow" id="top">
-        <div className="empty column is-3"></div>
-        <header className="column is-4">
-          <h1>Edward JieHao Huang</h1>
-          <h2>Full Stack Web Developer and Digital Artist</h2>
-          <hr/>
-          <p className="quote">
-            Striving for perfection and a keen eye for detail,<br/>
-            I promise that I won't disappoint or fail.<br/>
-            A passion for creativity and design,<br/>
+      <a href={item.link} target="_blank" rel="noreferrer noopener">
+        <img src={item.icon} alt={`${item.name} link`} className="footerIcon" />
+      </a>
+    )
+  }
+
+  // Render
+  // =============================================================
+  return (
+    <div className="columns centered title reverseRow" id="top">
+      <div className="empty column is-3"></div>
+      <header className="column is-4">
+        <h1>Edward JieHao Huang</h1>
+        <h2>Full Stack Web Developer and Digital Artist</h2>
+        <hr />
+        <p className="quote">
+          Striving for perfection and a keen eye for detail,<br />
+            I promise that I won't disappoint or fail.<br />
+            A passion for creativity and design,<br />
             I ensure my work will be divine.
           </p>
-          <hr/>
-          <div className="singleLine">
-              icons
-          </div>
-        </header>
-        <div className="column is-4">
-          <img src={headshot} alt="Headshot of Edward Huang" id="profilePicture"/>
+        <hr />
+        <div className="singleLine">
+          {contactLinks.map(mapContacts)}
+          <a href="mailto:ejhuang.2015@gmail.com">
+            <img src={emailIcon} alt="Email Link" className="footerIcon"/>
+          </a>
         </div>
+      </header>
+      <div className="column is-4">
+        <img src={headshot} alt="Headshot of Edward Huang" id="profilePicture" />
       </div>
-    )
+    </div>
+  )
 }
 
+// Exports
+// =============================================================
 export default Billboard;
